5fffcb6cfd2b5d33dff0f8722423a7ec0cd246f0
Andrey Murilev <94217327+AndySter@users.noreply.github.com>
2022-03-03 21:05:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,53 @@
 
 
 double pown(double value, uint16_t n) {
-
+    double nvalue = value;
+    for (int i = 1; i < n; i++) {
+        value *= nvalue;         
+    }
+    return value;
 }
 
 uint64_t fact(uint16_t n) {
-
+    uint64_t ft = 1;
+    while (n != 0) {
+      ft *= n;
+      n--;
+    }
+    return ft;
 }
 
 double calcItem(double x, uint16_t n) {
-
+    double numb = pown(x, n)/fact(n);
+    return numb;
 }
 
 double expn(double x, uint16_t count) {
-
+    double ex = 0;
+    int ch = 0;
+    while (ch != count) {
+        ex += calcItem(x, ch);
+        ch++;
+    }
+    return ex;
 }
 
 double sinn(double x, uint16_t count) {
-
+    double si;
+    uint16_t sich = 1;
+    while (sich != count) {
+        si += pown(-1, sich-1)*calcItem(x, 2*sich-1);
+        sich++;
+    }
+    return si;
 }
 
 double cosn(double x, uint16_t count) {
-
+    double co;
+    uint16_t coch = 1;
+    while (coch != count) {
+        co += pown(-1, coch-1)*calcItem(x, 2*coch-2);
+        coch++;
+    }
+    return co;
 }
